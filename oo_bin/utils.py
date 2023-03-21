109dc8dcb34245e362a94f8dafd88feb38df377f
Tjaart van der Walt <git@tjaart.org>
2023-03-21 14:08:29 -0700

    Improve browser detection for mac (#14)
    Handles firefox installed with homebrew or the store

--- oo_bin/utils.py
+++ oo_bin/utils.py
@@ -3,3 +3,11 @@ from platform import uname
 
 def is_wsl():
     return "microsoft-standard" in uname().release
+
+
+def is_mac():
+    return "Darwin" in uname().system
+
+
+def is_linux():
+    return "Linux" in uname().system
