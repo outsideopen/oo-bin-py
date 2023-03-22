62e542191ae0ab8677e7bf02e773030af9fce796
Tjaart van der Walt <git@tjaart.org>
2023-03-22 13:29:15 -0700

    Change config to use toml (#22)
    * Use tunnels.toml file
    
    * Update install script to convert config to toml

--- install.sh
+++ install.sh
@@ -3,51 +3,83 @@
 set -e
 
 function make_config {
-    mkdir -p $HOME/.config/oo_bin
-    touch $HOME/.config/oo_bin/tunnels.conf
+	mkdir -p $HOME/.config/oo_bin
+	touch $HOME/.config/oo_bin/tunnels.conf
 }
 
 function install {
-    VERSION=$(curl -L https://api.github.com/repos/outsideopen/oo-bin-py/releases/latest | grep tag_name | grep -Eo '[0-9]\.[0-9]\.[0-9]')
-    curl -LJO https://github.com/outsideopen/oo-bin-py/releases/download/${VERSION}/oo_bin-${VERSION}-py3-none-any.whl
+	VERSION=$(curl -L https://api.github.com/repos/outsideopen/oo-bin-py/releases/latest | grep tag_name | grep -Eo '[0-9]\.[0-9]\.[0-9]')
+	curl -LJO https://github.com/outsideopen/oo-bin-py/releases/download/${VERSION}/oo_bin-${VERSION}-py3-none-any.whl
 
-    pip3 install ./"oo_bin-${VERSION}-py3-none-any.whl"
-    rm oo_bin-${VERSION}-py3-none-any.whl
+	pip3 install ./"oo_bin-${VERSION}-py3-none-any.whl"
+	rm oo_bin-${VERSION}-py3-none-any.whl
 }
 
 function bash_add_local_bin_to_path {
-    echo ''
+	echo ''
 
-    if cat $HOME/.bashrc | grep -q 'PATH=$PATH:$HOME/.local/bin'
-    then
-        echo '`$HOME/.local/bin` is already added to your path. Nothing to be done'
-    else
-        echo 'Adding `PATH=$PATH:$HOME/.local/bin` to ~/.bashrc' 
-        export PATH=$PATH:$HOME/.local/bin
-        echo 'PATH=$PATH:$HOME/.local/bin' >> $HOME/.bashrc
-    fi
+	if cat $HOME/.bashrc | grep -q 'PATH=$PATH:$HOME/.local/bin'; then
+		echo '`$HOME/.local/bin` is already added to your path. Nothing to be done'
+	else
+		echo 'Adding `PATH=$PATH:$HOME/.local/bin` to ~/.bashrc'
+		export PATH=$PATH:$HOME/.local/bin
+		echo 'PATH=$PATH:$HOME/.local/bin' >>$HOME/.bashrc
+	fi
 }
 
 function bash_add_argscomplete {
-    echo ''
+	echo ''
 
-    if cat $HOME/.bashrc | grep -q 'source $HOME/.bash_completion/python-argcomplete'
-    then
-        echo '`python-argcomplete` is already sourced in your bashrc. Nothing to be done'
-    else
-        # Activate command line completion
-        mkdir -p $HOME/.bash_completion
-        activate-global-python-argcomplete --dest $HOME/.bash_completion
+	if cat $HOME/.bashrc | grep -q 'source $HOME/.bash_completion/python-argcomplete'; then
+		echo '`python-argcomplete` is already sourced in your bashrc. Nothing to be done'
+	else
+		# Activate command line completion
+		mkdir -p $HOME/.bash_completion
+		activate-global-python-argcomplete --dest $HOME/.bash_completion
 
-        echo 'Sourcing `python-argcomplete` in ~/.bashrc' 
-        # Temporarily add
-        source $HOME/.bash_completion/python-argcomplete
-        # Permanently add
-        echo 'source $HOME/.bash_completion/python-argcomplete' >> $HOME/.bashrc
-    fi
+		echo 'Sourcing `python-argcomplete` in ~/.bashrc'
+		# Temporarily add
+		source $HOME/.bash_completion/python-argcomplete
+		# Permanently add
+		echo 'source $HOME/.bash_completion/python-argcomplete' >>$HOME/.bashrc
+	fi
+}
+
+function convert_tunnels_conf {
+	OLD_CONFIG=$HOME/.config/oo_bin/tunnels.conf
+	NEW_CONFIG=$HOME/.config/oo_bin/tunnels.toml
+
+	if [ -f "$OLD_CONFIG" ]; then
+		while IFS=',' read -r NAME JUMP_HOST URLS || [ -n "$line" ]; do
+			if [ "$NAME" == "Location" ]; then continue; fi
+			echo "[${NAME}]" >>$NEW_CONFIG
+			echo "jump_host = '${JUMP_HOST}'" >>$NEW_CONFIG
+
+			echo "${JUMP_HOST}"
+			FIRST=true
+			URL_STRING="["
+			for URL in ${URLS//$'\r'/}; do
+				if [ "$FIRST" = true ]; then
+					URL_STRING="$URL_STRING'${URL}'"
+					FIRST=false
+				else
+					URL_STRING="$URL_STRING, '${URL}'"
+				fi
+			done
+			URL_STRING="$URL_STRING]"
+
+			echo "urls = ${URL_STRING}" >>$NEW_CONFIG
+			echo "" >>$NEW_CONFIG
+		done <$OLD_CONFIG
+
+		echo "converted $OLD_CONFIG to $NEW_CONFIG"
+		mv $OLD_CONFIG $OLD_CONFIG.bak
+		echo "renamed $OLD_CONFIG to $OLD_CONFIG.bak"
+	fi
 }
 
 make_config
 install
 bash_add_local_bin_to_path
 bash_add_argscomplete
+convert_tunnels_conf
