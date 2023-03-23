69c711ec2658739542f995e49d87bda6a500c48e
Tjaart van der Walt <git@tjaart.org>
2023-03-23 16:59:10 -0700

    Download config from a remote file

--- oo_bin/utils.py
+++ oo_bin/utils.py
@@ -1,4 +1,10 @@
+import shutil
 from platform import uname
+
+import requests
+from colorama import Fore
+
+from oo_bin.config import backup_tunnels_config, main_config, tunnels_config_path
 
 
 def is_wsl():
@@ -11,3 +17,22 @@ def is_mac():
 
 def is_linux():
     return "Linux" in uname().system
+
+
+def update_tunnels_config():
+    config = main_config()
+
+    update = config.get("tunnels", {}).get("update", None)
+
+    if update:
+        backup_tunnels_config()
+
+        url = update.get("url")
+        username = update.get("username")
+        password = update.get("password")
+
+        with requests.get(url, auth=(username, password), stream=True) as r:
+            r.raise_for_status()
+            with open(tunnels_config_path, "wb") as f:
+                shutil.copyfileobj(r.raw, f)
+        print(Fore.GREEN + f"Your configuration has been updated from {url}")
