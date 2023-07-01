f41de34d1cc3592eb2fe644e38c9c522696cc973
Tjaart van der Walt <git@tjaart.org>
2023-06-30 17:20:25 -0700

    Add tunnels tests (#181)

--- tests/oo_bin/tunnels/test_rdp.py
+++ tests/oo_bin/tunnels/test_rdp.py
@@ -1,12 +1,10 @@
 import os
 from pathlib import Path
 
-import pytest
-
-from oo_bin.tunnels import Socks
+from oo_bin.tunnels import Rdp
 
 
-class TestSocks:
+class TestRdp:
     def test_cmd(self, mocker):
         mocker.patch(
             "oo_bin.config.main_config_path",
@@ -20,16 +18,31 @@ class TestSocks:
                 Path(__file__).parent.parent.parent, "test_config", "tunnels.toml"
             ),
         )
-        socks = Socks("foo")
-        print(socks._cmd)
-
-        assert socks._cmd == [
+        rdp1 = Rdp("foo", "first_rdp")
+        assert rdp1._cmd == [
             "autossh",
             "-N",
             "-M",
             "0",
-            "-D",
-            "2080",
+            "-L",
+            "60001:192.168.1.1:3389",
+            "-o",
+            "ServerAliveInterval=3",
+            "-o",
+            "ServerAliveCountMax=30",
+            "-F",
+            "/my/ssh/config/path",
+            "foo.example.com",
+        ]
+
+        rdp2 = Rdp("foo", "second_rdp")
+        assert rdp2._cmd == [
+            "autossh",
+            "-N",
+            "-M",
+            "0",
+            "-L",
+            "60002:192.168.1.2:3389",
             "-o",
             "ServerAliveInterval=3",
             "-o",
