7cc3178685b9d54b5045b112666fc6a31ab3020e
Tjaart van der Walt <git@tjaart.org>
2023-06-30 17:14:45 -0700

    Add tunnels tests

--- tests/oo_bin/tunnels/test_vnc.py
+++ tests/oo_bin/tunnels/test_vnc.py
@@ -1,12 +1,10 @@
 import os
 from pathlib import Path
 
-import pytest
-
-from oo_bin.tunnels import Socks
+from oo_bin.tunnels import Vnc
 
 
-class TestSocks:
+class TestVnc:
     def test_cmd(self, mocker):
         mocker.patch(
             "oo_bin.config.main_config_path",
@@ -20,16 +18,31 @@ class TestSocks:
                 Path(__file__).parent.parent.parent, "test_config", "tunnels.toml"
             ),
         )
-        socks = Socks("foo")
-        print(socks._cmd)
-
-        assert socks._cmd == [
+        rdp1 = Vnc("foo", "first_vnc")
+        assert rdp1._cmd == [
             "autossh",
             "-N",
             "-M",
             "0",
-            "-D",
-            "2080",
+            "-L",
+            "60011:192.168.2.1:5900",
+            "-o",
+            "ServerAliveInterval=3",
+            "-o",
+            "ServerAliveCountMax=30",
+            "-F",
+            "/my/ssh/config/path",
+            "foo.example.com",
+        ]
+
+        rdp2 = Vnc("foo", "second_vnc")
+        assert rdp2._cmd == [
+            "autossh",
+            "-N",
+            "-M",
+            "0",
+            "-L",
+            "60012:192.168.2.2:5900",
             "-o",
             "ServerAliveInterval=3",
             "-o",
