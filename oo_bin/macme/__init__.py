bf1dae017cc6d8322e62240ca2c3f19a3ebc28d6
Tjaart van der Walt <git@tjaart.org>
2023-08-15 15:08:06 -0700

    Handle macvendors 404 errors more correctly

--- oo_bin/macme/__init__.py
+++ oo_bin/macme/__init__.py
@@ -6,9 +6,16 @@ class Macme:
         self.mac_address = mac_address
 
     def __lookup__(self):
-        with requests.get(f"http://api.macvendors.com/{self.mac_address}") as r:
-            r.raise_for_status()
-            return r.text
+        try:
+            with requests.get(f"http://api.macvendors.com/{self.mac_address}") as r:
+                r.raise_for_status()
+                return r.text
+        except requests.exceptions.HTTPError as e:
+            if e.response.status_code == 404:
+                print("The vendor could not be identified.")
+            else:
+                raise e
+            return ""
 
     def __str__(self):
         return self.__lookup__()
