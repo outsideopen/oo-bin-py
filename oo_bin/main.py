19b9826c5f18a25f87ec49e5ef6d3ba5233b22a5
Tjaart van der Walt <git@tjaart.org>
2023-08-14 17:30:22 -0700

    Save stack trace to file, and print only error messages (#229)
    * Save stack trace to file, and print only error messages
    
    * Add .python-version for CI

--- oo_bin/main.py
+++ oo_bin/main.py
@@ -1,9 +1,16 @@
+import os
 import sys
+import traceback
+from pathlib import Path
+from time import time
 
 import click
 import colorama
+import sentry_sdk
+from xdg import BaseDirectory
 
 from oo_bin import __version__
+from oo_bin.config import main_config
 from oo_bin.dnsme.command import dnsme
 from oo_bin.errors import OOBinError
 from oo_bin.hexme.command import hexme
@@ -12,9 +19,6 @@ from oo_bin.ping.command import ping
 from oo_bin.ssh.command import ssh
 from oo_bin.tunnels.command import rdp, tunnels, vnc
 from oo_bin.utils import auto_update, update_package, update_tunnels_config
-from oo_bin.config import main_config
-
-import sentry_sdk
 
 dsn = main_config().get("sentry", {}).get("dsn", None)
 
@@ -52,10 +56,32 @@ cli.add_command(vnc)
 
 
 def main():
-    auto_update()
-
     try:
+        auto_update()
         cli()
     except OOBinError as e:
+        # Handled errors, these errors should not be uploaded to sentry
         print(colorama.Fore.RED + f"Error: {e}", file=sys.stderr)
+        sys.exit(1)
+    except Exception as e:
+        # Unhandled errors, log to file, or upload to sentry
+        if dsn:
+            sentry_sdk.capture_exception(e)
+        else:
+            error_path = Path(
+                os.path.join(
+                    BaseDirectory.save_data_path("oo_bin"), f"error_{int(time())}"
+                )
+            )
+            with open(error_path, "w") as f:
+                e_type, e_val, e_tb = sys.exc_info()
+                traceback.print_exception(e_type, e_val, e_tb, file=f)
+
+            report_error = f"""Report this error by sending a Slack message to the OO #dev channel.
+Attach the file: {error_path}"""
+
+        print(colorama.Fore.RED + f"Error: {e}", file=sys.stderr)
+
+        if report_error:
+            print(f"\n{report_error}")
         sys.exit(1)
