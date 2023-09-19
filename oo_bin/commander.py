79a30fdfd41c702e308cea362a0132ded898fc21
David Lundgren <dlundgren@syberisle.net>
2023-09-19 10:11:32 -0500

    Load commands dynamically
    This also allows a user to have their own commands in ~/.local/share/oo_bin/shims

---
+++ oo_bin/commander.py
@@ -0,0 +1,73 @@
+import importlib
+import importlib.util
+from pathlib import Path
+
+
+class Commander:
+    """Not quite Cobra, but it snakes it's way through paths to find the common command format"""
+
+    def __init__(self, path):
+        self.commands = {}
+        self.load_from_path(path)
+
+    def find_in_path(self, path):
+        shim = "shims" in path
+        cmds = {}
+        p = Path(path)
+
+        def filename(file):
+            return (
+                str(file).replace(f"{path}/", "").replace(".py", "").replace("/", ".")
+            )
+
+        for cmd_file in p.glob("*/command/*.py"):
+            module_name = filename(cmd_file)
+            if "__init__" not in module_name:
+                cmds[module_name] = (
+                    module_name.split(".")[-1],
+                    str(cmd_file) if shim else None,
+                )
+
+        for cmd_file in p.glob("*/command.py"):
+            module_name = filename(cmd_file)
+            cmds[module_name] = (
+                module_name.split(".", 1)[0],
+                str(cmd_file) if shim else None,
+            )
+
+        return cmds
+
+    def load_from_path(self, path):
+        cmds = self.find_in_path(path)
+        for module_name in cmds:
+            (cmd_name, file) = cmds[module_name]
+            if file:
+                spec = importlib.util.spec_from_file_location(
+                    f"shim.{module_name}", file
+                )
+                module = importlib.util.module_from_spec(spec)
+                spec.loader.exec_module(module)
+            else:
+                module = importlib.import_module(f".{module_name}", "oo_bin")
+
+            if hasattr(module, "replaces"):
+                replaces = getattr(module, "replaces")
+                for name in replaces:
+                    if hasattr(module, replaces[name]):
+                        self.commands[name] = getattr(module, replaces[name])
+                    else:
+                        raise Exception(
+                            f"ERROR: could not load `{replaces[name]}` from `{module_name}`"
+                        )
+
+            if hasattr(module, cmd_name):
+                self.commands[module_name] = getattr(module, cmd_name)
+
+    def register(self, cli):
+        for cmd in self.commands:
+            cli.add_command(self.commands[cmd])
+
+    def load_from_paths(self, *paths):
+        # these paths are assumed to be shims as the canonical commands are registered above
+        for path in paths:
+            self.load_from_path(path)
