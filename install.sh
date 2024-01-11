fd33a09ea4d6461c99c8b49d74e35651537e7887
Andy R <asrusch@gmail.com>
2024-01-11 11:07:47 -0800

    Update install.sh
    special needs

--- install.sh
+++ install.sh
@@ -65,7 +65,7 @@ function install {
 	fi
 
 	curl -LJO $DOWNLOAD_URL
-	pip3 install --force-reinstall --break-system-packages ./"$FILENAME"
+	pip3 install --force-reinstall ./"$FILENAME" --break-system-packages
 	rm "$FILENAME"
 }
 
