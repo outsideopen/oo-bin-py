89ed00ee560e5f2ab771ec22e29b65d297d2397c
George Speake <george.speake@gmail.com>
2026-02-10 13:39:05 -0800

    updates to use main_config for keyme file

--- oo_bin/keyme/__init__.py
+++ oo_bin/keyme/__init__.py
@@ -5,23 +5,21 @@ import colorama
 import pyperclip as pc
 import tomli_w
 
+from oo_bin.config import main_config, save_main_config
+
 
 class Keyme:
     def __init__(self, file, save=False):
-        self.config_path = os.path.join(os.getcwd(), "keyme.toml")
+        config = main_config()
         self.file = None
 
         if file:
             self.file = file
 
             if save:
-                self.__save_config()
+                self.__save_file_to_config()
         else:
-            if os.path.isfile(self.config_path):
-                with open(self.config_path, "rb") as f:
-                    config = tomllib.load(f)
-
-                    self.file = config.get("file", "")
+            self.file = config.get("keyme", {}).get("file", None)
 
     def __str__(self):
         colorama.init(autoreset=True)
@@ -39,9 +37,8 @@ class Keyme:
 
         return ""
 
-    def __save_config(self):
-        data_to_save = {
-            "file": self.file,
-        }
-        with open(self.config_path, "wb") as f:
-            tomli_w.dump(data_to_save, f)
+    def __save_file_to_config(self):
+        data_to_save = {"file": self.file}
+        config = main_config()
+        config["keyme"] = data_to_save
+        save_main_config(config)
