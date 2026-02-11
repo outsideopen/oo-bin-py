a372d5ba9ea56671329d8acdc6cdf02a500d3791
George Speake <george.speake@gmail.com>
2026-02-11 08:40:10 -0800

    updates to address review issues

--- oo_bin/keyme/__init__.py
+++ oo_bin/keyme/__init__.py
@@ -9,17 +9,10 @@ from oo_bin.config import main_config, save_main_config
 
 
 class Keyme:
-    def __init__(self, file, save=False):
-        config = main_config()
-        self.file = None
-
-        if file:
-            self.file = file
-
-            if save:
-                self.__save_file_to_config()
-        else:
-            self.file = config.get("keyme", {}).get("file", None)
+    def __init__(self, file=None, save=False):
+        self.file = file if file else main_config().get("keyme", {}).get("file", None)
+        if file and save:
+            self.__save_file_to_config()
 
     def __str__(self):
         colorama.init(autoreset=True)
@@ -30,15 +23,11 @@ class Keyme:
                     pc.copy(content)
                     return f"{colorama.Fore.GREEN}SSH public key copied to clipboard from {colorama.Style.BRIGHT}{self.file}"
             else:
-                return f"{colorama.Fore.RED}Provided file does not exist: {colorama.Style.BRIGHT}{self.file}"
+                return f"{colorama.Fore.RED}Requested file does not exist: {colorama.Style.BRIGHT}{self.file}"
 
-        else:
-            return f"{colorama.Back.YELLOW}{colorama.Fore.BLACK}No key file provided. Add one with the {colorama.Style.BRIGHT}{colorama.Fore.BLUE}--file{colorama.Style.RESET_ALL}{colorama.Back.YELLOW}{colorama.Fore.BLACK} option. Save it for next time with the {colorama.Fore.BLUE}{colorama.Style.BRIGHT}--save{colorama.Style.RESET_ALL}{colorama.Back.YELLOW}{colorama.Fore.BLACK} flag."
-
-        return ""
+        return f"{colorama.Back.YELLOW}{colorama.Fore.BLACK}No key file provided. Add one with the {colorama.Style.BRIGHT}{colorama.Fore.BLUE}--file{colorama.Style.RESET_ALL}{colorama.Back.YELLOW}{colorama.Fore.BLACK} option. Save it for next time with the {colorama.Fore.BLUE}{colorama.Style.BRIGHT}--save{colorama.Style.RESET_ALL}{colorama.Back.YELLOW}{colorama.Fore.BLACK} flag."
 
     def __save_file_to_config(self):
-        data_to_save = {"file": self.file}
         config = main_config()
-        config["keyme"] = data_to_save
+        config["keyme"] = {"file": self.file}
         save_main_config(config)
--- oo_bin/keyme/command.py
+++ oo_bin/keyme/command.py
@@ -4,7 +4,7 @@ from oo_bin.keyme import Keyme
 
 
 @click.command(help="Copies public SSH key to the clipboard")
-@click.option("--file", default="", help="Public SSH key file")
+@click.option("--file", help="Public SSH key file")
 @click.option("--save", is_flag=True, help="Save path to ssh public key")
 def keyme(file, save):
     keyme = Keyme(file, save)
